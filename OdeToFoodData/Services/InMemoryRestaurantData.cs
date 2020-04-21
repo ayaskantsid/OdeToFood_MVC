a5e7e739ee3aff74c58f619502e1f07a121ed4e0
Ayaskant Siddharth <ishugzb1998@gmail.com>
2020-04-22 02:26:07 +0530

    modified directory of Data Access Layer

---
+++ OdeToFoodData/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFoodData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFoodData.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id=1, Name="Indian Accent", Cuisine=CuisineType.Indian},
+                new Restaurant{Id=2, Name="Shang Palace", Cuisine=CuisineType.French},
+                new Restaurant{Id=3, Name="Wasabi", Cuisine=CuisineType.Italian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(i => i.Name);
+        }
+    }
+}
